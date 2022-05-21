e22f22ed555d6ff3b8c6c879d5dfe2da92cfadcc
BOBILLEChristophe <38000184+BOBILLEChristophe@users.noreply.github.com>
2022-05-21 20:53:58 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,18 @@ class Aiguille : public Servo
 {
 
 private:
-  uint8_t id;
-  uint8_t servoPin;
-  uint8_t railPinAig; // pins allocated to rail volt status for turnout
-  uint8_t togPinAig;  // choose the input pin of toggle switches for simple turnout
-  uint8_t ledPinAig;  // pins allocated to Led control for simple turnout
-  uint8_t relPinAig;
+  uint8_t  id;
+  uint8_t  servoPin;
+  uint8_t  railPinAig; // pins allocated to rail volt status for turnout
+  uint8_t  togPinAig;  // choose the input pin of toggle switches for simple turnout
+  uint8_t  ledPinAig;  // pins allocated to Led control for simple turnout
+  uint8_t  relPinAig;
   uint16_t minPosition;
   uint16_t maxPosition;
-  uint8_t posAig;
-  uint8_t dirAig;
-  uint8_t togValAig;
-  uint8_t railValAig;
+  uint8_t  posAig;
+  uint8_t  dirAig;
+  uint8_t  togValAig;
+  uint8_t  railValAig; // fin de course
   uint64_t lastMoveTime;
   uint64_t tempo;
 
@@ -29,16 +29,16 @@ public:
 
 // Constructor
 Aiguille::Aiguille() : id(0),
-                       servoPin(0),
-                       railPinAig(0),
-                       togPinAig(0),
-                       ledPinAig(0),
-                       relPinAig(0),
-                       minPosition(0),
-                       maxPosition(0),
+                       servoPin(255),
+                       railPinAig(255),
+                       togPinAig(255),
+                       ledPinAig(255),
+                       relPinAig(255),
+                       minPosition(1500),
+                       maxPosition(1500),
                        posAig(0),
                        dirAig(0),
-                       tempo(10)
+                       tempo(5)
 {
 }
 
@@ -63,18 +63,13 @@ void Aiguille::setup(uint8_t a, uint8_t b, uint8_t c, uint8_t d, uint8_t e, uint
 void Aiguille::test()
 {
   this->attach(servoPin);
-  for (byte i = 0; i < 3; i++)
-  {
-    this->writeMicroseconds(minPosition);
-    Serial.println("goto min");
-    delay(1000);
-    this->writeMicroseconds(maxPosition);
-    Serial.println("goto max");
-    delay(1000);
-  }
+  this->writeMicroseconds(minPosition);
+  delay(100);
+  this->writeMicroseconds(maxPosition);
+  delay(100);
+  posAig = maxPosition;
   this->detach();
 }
-
 
 void Aiguille::loop()
 {
@@ -121,20 +116,25 @@ void Aiguille::loop()
   }
 }
 
-#define NB_SERVO 2
+#define NOPIN 255
+#define NB_SERVO 6
 Aiguille aiguille[NB_SERVO]; // Tableau contenant 'NB_SERVO' instances (objets)
 
 void setup()
 {
-  // Serial.begin(9600); // start the serial port
-  Serial.begin(115200); // start the serial port
+  Serial.begin(9600); // start the serial port
+  //Serial.begin(115200); // start the serial port
 
   // Setup de chaque instance
-  // id, servoPin, railPinAig, togPinAig, ledPinAig, relPinAig, minPosition, maxPosition, (dirAig)
-  aiguille[0].setup(5, 6, 30, 39, 48, 65, 750, 2200, 0);
-  aiguille[1].setup(6, 7, 32, 41, 50, 66, 1250, 1750, 0);
-  
-   for (auto el : aiguille)
+  //id, servoPin, railPinAig, togPinAig, ledPinAig, relPinAig, minPosition, maxPosition, (dirAig)
+  aiguille[0].setup(1, 2, 49,     31, 40, 65, 750,  2200, 0);
+  aiguille[1].setup(2, 3, 51,     33, 42, 66, 1250, 1750, 0);
+  aiguille[2].setup(3, 4, 53,     35, 44, 65, 1400, 1800, 0);
+  aiguille[3].setup(4, 5, NOPIN,  37, 46, 66, 1250, 1750, 0);
+  aiguille[4].setup(5, 6, NOPIN,  39, 48, 65, 1500, 1500, 0);
+  aiguille[5].setup(6, 11, NOPIN, 41, 50, 66, 1300, 2200, 0);
+
+  for (auto el : aiguille)
     el.test();
 }
 
