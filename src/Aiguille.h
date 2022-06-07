076036511f088698441c2c4138bbe1086243c83b
BOBILLEChristophe <38000184+BOBILLEChristophe@users.noreply.github.com>
2022-06-07 11:37:44 +0200

    Update Aiguille.h

--- src/Aiguille.h
+++ src/Aiguille.h
@@ -10,36 +10,36 @@
 #include <EEPROM.h>
 #include <Servo.h>
 
-#define NOPIN 255
+
 
 class Aiguille : public Servo
 {
 
-private:
-  uint8_t  id;
-  uint8_t  servoPin;
-  uint8_t  railPinAig; // pins allocated to rail volt status for turnout
-  uint8_t  togPinAig;  // choose the input pin of toggle switches for simple turnout
-  uint8_t  ledPinAig;  // pins allocated to Led control for simple turnout
-  uint8_t  relPinAig;
-  uint16_t minPosition;
-  uint16_t maxPosition;
-  uint16_t posAig;
-  uint8_t  dirAig;
-  uint8_t  togValAig;
-  uint8_t  railValAig; // fin de course
-  uint64_t lastMoveTime;
-  uint16_t tempo;
+  private:
+    uint8_t  id;
+    uint8_t  servoPin;
+    uint8_t  togPinAig;  // choose the input pin of toggle switches for simple turnout
+    uint8_t  ledPinAig;  // pins allocated to Led control for simple turnout
+    uint16_t minPosition;
+    uint16_t maxPosition;
+    uint16_t posAig;
+    uint8_t  dirAig;
+    uint8_t  togValAig;
+    uint64_t lastMoveTime;
+    uint16_t tempo;
 
-public:
-  Aiguille(); // Constructor
-  void setup(uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint16_t, uint16_t, uint8_t);
-  void loop();
-  void test();
-  void reglageServo(uint8_t);
-  void saveMinPos();
-  void saveMaxPos();
-  void writePosServo();
+
+
+
+  public:
+    Aiguille(); // Constructor
+    void setup(uint8_t, uint8_t, uint8_t, uint8_t, uint16_t, uint16_t, uint8_t);
+    void loop();
+    void test();
+    void reglageServo(uint8_t);
+    void saveMinPos();
+    void saveMaxPos();
+    void writePosServo();
 };
 
 #endif
